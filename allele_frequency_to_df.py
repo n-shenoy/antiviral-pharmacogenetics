4edf2d4240269d62c512430a30897feb6e8437a1
n-shenoy <51956619+n-shenoy@users.noreply.github.com>
2022-10-15 15:44:57 -0400

    added Python file

---
+++ allele_frequency_to_df.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_excel("41586_2019_1793_MOESM3_ESM.xlsx", 
+                    sheet_name = 'S4g-Pharmacogenomic SNPs',
+                    header = 2, index_col = [0,1])
+print(df)
+
+df.reset_index(level = [0,1], inplace = True)
+df.drop(labels=0, axis=0, inplace=True)
+print(df)
+
+df.to_csv('allele_frequencies.csv')
